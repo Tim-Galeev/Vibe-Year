a36d69a5a1f586fd9dd4a83ca6aff99449d9ff6e
Галеев Тимур Маратович <tgaleev@i-novus.ru>
2025-12-17 16:24:58 +0300

    font emoji

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765946100|250416962';
+const CACHE_VERSION = '1765977729|53786860';
 /** @type {string} */
 const CACHE_PREFIX = 'Новогодний Забро-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
