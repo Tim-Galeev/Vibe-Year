b91ce29333bfb331af14afaa57cae6f7564fe0d1
TimGaleev <tim-galeev@outlook.com>
2025-12-19 08:03:54 +0300

    mobilr

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766096543|927605033';
+const CACHE_VERSION = '1766120584|309694053';
 /** @type {string} */
 const CACHE_PREFIX = 'Vibe Year-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
