286c27242e66dd20740fded2040e6090c9360de7
Галеев Тимур Маратович <tgaleev@i-novus.ru>
2025-12-19 11:33:08 +0300

    reexport

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766132424|2313938286';
+const CACHE_VERSION = '1766133177|3067534977';
 /** @type {string} */
 const CACHE_PREFIX = 'Vibe Year-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
