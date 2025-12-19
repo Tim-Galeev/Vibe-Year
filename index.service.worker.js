bfcbd1e715f1561995f0fe6f10cc52fab77f5099
Галеев Тимур Маратович <tgaleev@i-novus.ru>
2025-12-19 11:53:35 +0300

    buttons

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1766133177|3067534977';
+const CACHE_VERSION = '1766134401|4290969905';
 /** @type {string} */
 const CACHE_PREFIX = 'Vibe Year-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
